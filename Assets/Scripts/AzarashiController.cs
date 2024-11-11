67cc206cb76563f77ca769116068515343c054bf
Ishide <ishide.naoto1997@gmail.com>
2024-11-11 10:34:03 +0900

    操作キャラクター作成

--- Assets/Scripts/AzarashiController.cs
+++ Assets/Scripts/AzarashiController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class AzarashiController : MonoBehaviour
 {
+    Rigidbody2D rd2d;
+
+    public float maxHeight;
+    public float flapVelocity;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Awake() {
+        rd2d = GetComponent<Rigidbody2D>();    
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1") && transform.position.y < maxHeight){
+            flap();
+        }
+    }
+
+    public void flap(){
+        rd2d.velocity = new Vector2(0.0f,flapVelocity);
     }
 }
