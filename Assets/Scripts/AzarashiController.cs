96231961856b9981d04dc709ff7b55e6812b4ead
Ishide <ishide.naoto1997@gmail.com>
2024-11-11 10:45:16 +0900

    上昇下降時のアニメーション反映

--- Assets/Scripts/AzarashiController.cs
+++ Assets/Scripts/AzarashiController.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AzarashiController : MonoBehaviour
 {
     Rigidbody2D rd2d;
+    Animator animator;
+    float angle;
 
     public float maxHeight;
     public float flapVelocity;
+    public float relativeVelocityX;
+    public GameObject sprite;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void Awake() {
         rd2d = GetComponent<Rigidbody2D>();    
+        animator = sprite.GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
@@ -23,9 +25,18 @@ public class AzarashiController : MonoBehaviour
         if(Input.GetButtonDown("Fire1") && transform.position.y < maxHeight){
             flap();
         }
+
+        ApplyAngle();
+        animator.SetBool("flap",angle >= 0.0f);
     }
 
     public void flap(){
         rd2d.velocity = new Vector2(0.0f,flapVelocity);
+    }
+
+    void ApplyAngle(){
+        float targetAngle = Mathf.Atan2(rd2d.velocity.y,relativeVelocityX) * Mathf.Rad2Deg;
+        angle = Mathf.Lerp(angle,targetAngle,Time.deltaTime * 10.0f);
+        sprite.transform.localRotation = Quaternion.Euler(0.0f,0.0f,angle);
     }
 }
