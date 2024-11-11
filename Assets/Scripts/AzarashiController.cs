7b86376dee59102f06037b625eab0f72c4e148f5
Ishide <ishide.naoto1997@gmail.com>
2024-11-11 11:28:39 +0900

    ステージオブジェクト配置

--- Assets/Scripts/AzarashiController.cs
+++ Assets/Scripts/AzarashiController.cs
@@ -8,12 +8,17 @@ public class AzarashiController : MonoBehaviour
     Rigidbody2D rd2d;
     Animator animator;
     float angle;
+    bool isDead;
 
     public float maxHeight;
     public float flapVelocity;
     public float relativeVelocityX;
     public GameObject sprite;
     // Start is called before the first frame update
+
+    public bool IsDead(){
+        return isDead;
+    }
 
     void Awake() {
         rd2d = GetComponent<Rigidbody2D>();    
@@ -27,16 +32,30 @@ public class AzarashiController : MonoBehaviour
         }
 
         ApplyAngle();
-        animator.SetBool("flap",angle >= 0.0f);
+        animator.SetBool("flap",angle >= 0.0f && !isDead);
     }
 
     public void flap(){
+        if(isDead) return;
         rd2d.velocity = new Vector2(0.0f,flapVelocity);
     }
 
     void ApplyAngle(){
-        float targetAngle = Mathf.Atan2(rd2d.velocity.y,relativeVelocityX) * Mathf.Rad2Deg;
+        float targetAngle;
+
+        if(isDead){
+            targetAngle = 180.0f;
+        }else{
+            targetAngle = Mathf.Atan2(rd2d.velocity.y,relativeVelocityX) * Mathf.Rad2Deg;
+        }
+
+
         angle = Mathf.Lerp(angle,targetAngle,Time.deltaTime * 10.0f);
         sprite.transform.localRotation = Quaternion.Euler(0.0f,0.0f,angle);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(isDead) return;
+        isDead = true;
     }
 }
